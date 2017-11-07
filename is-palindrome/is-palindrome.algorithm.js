b55a135733362d5b7c26ffd5ebd35a003f5d3679
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2017-11-07 21:34:43 +0200

    Add isPalindrome algorithm

---
+++ is-palindrome/is-palindrome.algorithm.js
@@ -0,0 +1,17 @@
+function isPalindrome(string) {
+    string = string.toLowerCase();
+    let charactersArr = string.split('');
+    let validCharacters = 'abcdefghijklmnopqrsuvwxyz'.split('');
+
+    let lettersArr = [];
+    charactersArr.forEach(character => {
+        if (validCharacters.indexOf(character) > -1) lettersArr.push(character);
+    });
+
+    if (lettersArr.join('') === lettersArr.reverse().join('')) return true;
+    else return false;
+}
+
+console.log(isPalindrome("Madam, I'm Adam"));
+console.log(isPalindrome('race car'));
+console.log(isPalindrome('javascript'));
