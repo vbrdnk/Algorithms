ad551be4e47c01fa5283f8fcade17ec9a88dea8c
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2018-01-10 16:33:22 +0200

    add algorithm that finds max stock profit from array of prices

---
+++ max-stock-profit/max_stock_profit.algorithm.js
@@ -0,0 +1,28 @@
+function maxStockProfit(pricesArr) {
+    let maxProfit = -1;
+    let buyPrice = 0;
+    let sellPrice = 0;
+
+    let changeBuyPrice = true;
+
+    for (let i = 0; i < pricesArr.length; i++) {
+        if (changeBuyPrice) { 
+            buyPrice = pricesArr[i];
+        }
+        sellPrice = pricesArr[i + 1];
+
+        if (sellPrice < buyPrice) {
+            changeBuyPrice = true;
+        } else {
+            let tempProfit = sellPrice - buyPrice;
+            if (tempProfit > maxProfit) {
+                maxProfit = tempProfit;
+            }
+            changeBuyPrice = false;
+        }
+    }
+
+    return maxProfit;
+}
+
+console.log(maxStockProfit([32, 46, 26, 38, 40, 48, 42]));
